19115222ee3f78db4ff0b6b3112602ca48a28843
Dalton Coppage <61177291+dcoppage@users.noreply.github.com>
2022-04-14 13:22:22 -0700

    Update TickerScrape.py

--- Design Documents/Source/TickerScrape.py
+++ Design Documents/Source/TickerScrape.py
@@ -6,6 +6,26 @@ Ticker = yf.Ticker(Company)
 
 #print(Ticker.info.keys())
 
-# Company Sector
-print(Ticker.info['sector'], Ticker.info['trailingPE'], Ticker.info['beta'])
+print('\nCompany Information')
+print('-------------------')
+print('Company Sector: ',Ticker.info['sector'])
+print('Market CAP: ',Ticker.info['marketCap'])
 
+print('\nPrice Changes')
+print('---------------')
+print('Market Open Price: ',Ticker.info['regularMarketOpen'])
+print('Previous Close Price: ',Ticker.info['previousClose'])
+print('52 Week Change in Price: ',Ticker.info['52WeekChange'])
+print('Target Low Price: ',Ticker.info['targetLowPrice'])
+print('Target Median Price: ',Ticker.info['targetMedianPrice'])
+print('Target High Price: ',Ticker.info['targetHighPrice'])
+
+print('\nPerformance Outlook')
+print('---------------------')
+print('3-Year Beta: ',Ticker.info['beta3Year'])
+print('Current Beta: ',Ticker.info['beta'])
+print('Short Ratio: ',Ticker.info['shortRatio'])
+print('PEG Ratio: ',Ticker.info['pegRatio'])
+print('YTD Return: ',Ticker.info['ytdReturn'])
+print('Forward P/E: ',Ticker.info['forwardPE'])
+print('Trailing P/E: ',Ticker.info['trailingPE'])
