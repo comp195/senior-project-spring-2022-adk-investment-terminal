3543e5c55be5c7696ed62e4852d1f5056fca6ac8
dalt <d_coppage@u.pacific.edu>
2022-05-05 22:32:07 -0700

    New Buttons

---
+++ Design Documents/Source/Charts.py
@@ -0,0 +1,60 @@
+import yfinance as yf
+#df = yf.download("MSFT", start="2015-01-01", end="2021-03-31", interval="1d")
+#df.head()
+msft = yf.Ticker("MSFT")
+
+# get stock info
+#print(msft.info)
+
+# get historical market data
+#hist = msft.history(period="max")
+
+# show actions (dividends, splits)
+#print(msft.actions)
+
+# show dividends
+#print(msft.dividends)
+
+# show splits
+#print(msft.splits)
+
+# show financials
+#print(msft.financials)
+#print(msft.quarterly_financials)
+
+# show major holders
+#print(msft.major_holders)
+
+# show institutional holders
+#print(msft.institutional_holders)
+
+# show balance sheet
+#print(msft.balance_sheet)
+msft.quarterly_balance_sheet
+
+# show cashflow
+msft.cashflow
+msft.quarterly_cashflow
+
+# show earnings
+msft.earnings
+msft.quarterly_earnings
+
+# show sustainability
+msft.sustainability
+
+# show analysts recommendations
+#print(msft.recommendations)
+
+# show next event (earnings, etc)
+msft.calendar
+
+# show ISIN code - *experimental*
+# ISIN = International Securities Identification Number
+msft.isin
+
+# show options expirations
+msft.options
+
+# show news
+msft.news
