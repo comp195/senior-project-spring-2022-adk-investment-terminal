f693652e82c9b01b0f4b1aa85650932dd62f363c
dalt <d_coppage@u.pacific.edu>
2022-05-05 23:32:52 -0700

    Financials

--- Design Documents/Source/Charts.py
+++ Design Documents/Source/Charts.py
@@ -10,31 +10,10 @@ msft = TickerScrape.Ticker
 #hist = msft.history(period="max")
 
 
-# show financials
-#print(msft.financials)
-#print(msft.quarterly_financials)
-
-
-# show balance sheet
-#print(msft.balance_sheet)
-msft.quarterly_balance_sheet
-
-# show cashflow
-msft.cashflow
-msft.quarterly_cashflow
-
-# show earnings
-msft.earnings
-msft.quarterly_earnings
 
 # show sustainability
 msft.sustainability
 
 
-# show ISIN code - *experimental*
-# ISIN = International Securities Identification Number
-msft.isin
 
-# show options expirations
-msft.options
 
