a35258d1b4849a618b11dcf77671bdf854e11acd
kunalbabbar1 <k_babbar@u.pacific.edu>
2022-05-07 03:57:52 -0700

    Final Push

--- Requirements /Source Code/Scatter.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import TickerScrape
-
-solvencyLT = [TickerScrape.beta, TickerScrape.peg]
-solvencyST = [TickerScrape.current, TickerScrape.quick]
-
-plt.scatter(solvencyLT, solvencyST)
-plt.show()
